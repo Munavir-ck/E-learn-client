181b53c6d8ce2db1c93ebcd5216bca18c76f4a77
Munavir <munavirokv@gmail.com>
2023-05-17 19:10:20 +0530

    edited

--- src/Components/Tutor/Booking_list.jsx
+++ src/Components/Tutor/Booking_list.jsx
@@ -68,7 +68,7 @@ function Booking_list() {
 
   const handleOnclick = (studentId) => {
 
-
+  console.log(studentId,11111111111111);
 
     const room = studentId
     console.log(room,2222);
@@ -310,23 +310,7 @@ function Booking_list() {
                   })}
                 </tbody>
               </table>
-              {/* <div
-						className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
-						<span className="text-xs xs:text-sm text-gray-900">
-                            Showing 1 to 4 of 50 Entries
-                        </span>
-						<div className="inline-flex mt-2 xs:mt-0">
-							<button
-                                className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
-                                Prev
-                            </button>
-							&nbsp; &nbsp;
-							<button
-                                className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r">
-                                Next
-                            </button>
-						</div>
-					</div> */}
+            
               
             </div>
           </div>
