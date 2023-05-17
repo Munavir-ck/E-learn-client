348bf91c2bdf520e78b887ac23e0f59bc79fb9a6
Munavir <munavirokv@gmail.com>
2023-05-17 19:37:42 +0530

    edited

--- src/Routes/Teacher.jsx
+++ src/Routes/Teacher.jsx
@@ -8,6 +8,7 @@ import BookingList_page from "../pages/Tutor/BookingList_page";
 import ChatRoomTutorPage from "../pages/Tutor/ChatRoomPage";
 import RoomPage from "../Components/screen/Room";
 import ErrorPage from '../Components/Admin/ErrorPage/ErrorPage';
+import RoomPage from "../Components/screen/Room";
 import { useSelector } from 'react-redux'
 
 
@@ -27,8 +28,8 @@ function Teacher() {
 
         <Route path="/manage_time" element={teacherToken? <Time_managePage />:<Navigate to={"/tutor"}/>} />
         <Route path="/booking_list" element={teacherToken?<BookingList_page />:<Navigate to={"/tutor"}/>} />
-        <Route path="/chat_room/:id" element={ teacherToken?<ChatPage />:<Navigate to={"/tutor"}/>} />
-        
+        <Route path="/room/:roomId" element={ teacherToken?<RoomPage />:<Navigate to={"/login"}/>} />
+        <Route path="/chat_room_tutor/:id" element={teacherToken?<ChatRoomTutorPage />:<Navigate to={"/tutor"}/>} />
         <Route path="/*"  element={<ErrorPage link={"/tutor/home"} />} />
       </Routes>
     </>
